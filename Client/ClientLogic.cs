189b1ae13246f0608bbfb92980f7b82f1bc506dd
Pedro Neves <pedro.santos.neves@tecnico.ulisboa.pt>
2023-09-25 12:12:13 +0100

    Fix: quality of code improvement

--- Client/ClientLogic.cs
+++ Client/ClientLogic.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using DADTKV_Client_Lib;
 public class ClientLogic
 {
@@ -16,240 +17,28 @@ public class ClientLogic
 	}
 	public void Transaction()
 	{
-        string newline = Console.ReadLine();
-        if (newline == null) { Console.WriteLine("ERROR: Invalid Transaction"); return; }
-		List<string> reads = new List<string>();
-		bool read = false;
-		List<DadInt> writes = new List<DadInt>();
+        string newline = Parse_Lib.Parse.ParseStringNoSpaces();
+        if (newline == "") { Console.WriteLine("ERROR: Invalid Transaction"); return; }
+		List<string> reads;
+		Dictionary<string,int> writes;
         int i = 0;
 		
-		while (i < newline.Length)
-		{
-			if (newline[i] == '(') 
-			{
-                i++;
-                if (i >= newline.Length)
-                {
-                    Console.WriteLine("ERROR: Bad Transaction Format");
-                    return;
-                }
-                while (newline[i] == ' ' || newline[i] == '\t')
-                {
-                    //verificar se e menor que length
-                    i++;
-                    if (i >= newline.Length)
-                    {
-                        Console.WriteLine("ERROR: Bad Transaction Format");
-                        return;
-                    }
-                }
-                if (!read)
-                {
-                    bool ask = false;
-                    bool com = false;
-                    if (newline[i] == ')') { read = true; }
-                    else if (newline[i] == '"')
-                    {
-                        string st = "";
-                        i++;
-                        if (i >= newline.Length)
-                        {
-                            Console.WriteLine("ERROR: Bad Transaction Format");
-                            return;
-                        }
-                        ask = true;
-                        while (!(newline[i] == ')' && !ask && !com))
-                        {
-                            if (ask && newline[i] == '"')
-                            {
-                                reads.Add(st);
-                                st = "";
-                                ask = false;
-                                com = false;
-                            }
-                            else if (ask)
-                            {
-                                st += newline[i];
-                            }
-                            else if (!com && newline[i] == ',')
-                            {
-                                com = true;
-                            }
-                            else if (com && newline[i] == '"')
-                            {
-                                ask = true;
-                            }
-                            else if (!(newline[i] == ' ' || newline[i] == '\t'))
-                            {
-                                Console.WriteLine("ERROR: Bad Read Transaction Format");
-                                return;
-                            }
-                            i++;
-                            if (i > newline.Length)
-                            {
-                                Console.WriteLine("ERROR: Bad Read Transaction Format");
-                                return;
-                            }
-                        }
-                        read = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("ERROR: Bad Read Transaction Format");
-                        return;
-                    }
-                    foreach (string s in reads) { Console.WriteLine(s); }
-                }
-                else
-                {
-                    int ask = 0;
-                    bool com = false;
-                    bool bra = false;
-                    bool numE = false;
-                    if (newline[i] == ')') { break; } // break's out of the cycle
-                    else if (newline[i] == '<')
-                    {
-                        string st = "";
-                        int val = -1;
-                        i++;
-                        if (i >= newline.Length)
-                        {
-                            Console.WriteLine("ERROR: Bad Transaction Format");
-                            return;
-                        }
-                        bra = true;
-                        while(newline[i] == ' ' || newline[i] == '\t')
-                        {
-                            //verificar se e menor que length
-                            i++;
-                            if (i >= newline.Length)
-                            {
-                                Console.WriteLine("ERROR: Bad Transaction Format");
-                                return;
-                            }
-                        }
-                        if (newline[i] != '"')
-                        {
-                            Console.WriteLine("ERROR: Bad Write Transaction Format");
-                            return;
-                        }
-                        ask = 1;
-                         i++;
-                        if (i >= newline.Length)
-                        {
-                            Console.WriteLine("ERROR: Bad Transaction Format");
-                            return;
-                        }
-                        while (!(newline[i] == ')' && ask==0 && !com && !bra))
-                        {
-                            //////////////////////////////////////
-                            ///
-                            if (bra)
-                            {
-                                if (ask==2 && com && val != -1 && newline[i] == '>')
-                                {
-                                    writes.Add(new DadInt(st, val));
-                                    st = "";
-                                    val = -1;
-                                    bra = false;
-                                    ask = 0;
-                                    com = false;
-                                    numE = false;
-                                }
-                                else if (ask == 1 && newline[i] == '"')
-                                {
-                                    ask = 2;
-                                }
-                                else if (ask == 1)
-                                {
-                                    st += newline[i];
-                                }
-                                else if (!com && ask == 2 && newline[i] == ',')
-                                {
-                                    com = true;
 
-                                }
-                                else if (com && ask == 2) 
-                                {
-                                    if (val == -1) 
-                                    {
-                                        if (Char.IsNumber(newline[i]))
-                                            val = newline[i]-'0';
 
-                                        else if (!(newline[i] == ' ' || newline[i] == '\t'))
-                                        {
-                                            Console.WriteLine("ERROR: Bad Write Transaction Format");
-                                            return;
-                                        }  
-                                    }
-                                    else if (Char.IsNumber(newline[i]) && !numE) val = val * 10 + newline[i]-'0';
-                                    else
-                                    {
-                                        if (!numE && val > -1 && (newline[i] == ' ' || newline[i] == '\t')) numE = true;
-                                        else
-                                        {
-                                            Console.WriteLine("ERROR: Bad Write Transaction Format");
-                                            return;
-                                        }
-
-                                    }
-                                }
-                                else if (!(newline[i] == ' ' || newline[i] == '\t'))
-                                {
-                                    if (!(ask == 0 && newline[i] == '"'))
-                                    {
-                                        Console.WriteLine("ERROR: Bad Write Transaction Format");
-                                        return;
-                                    }
-                                    ask = 1;
-                                }
-                            } 
-                            else if( !com && newline[i] == ',')
-                            {
-                                com = true;
-                            }
-                            else if( com && newline[i] == '<')
-                            {
-                                bra = true;
-                                com = false;
-                            }
-                            else if (!(newline[i] == ' ' || newline[i] == '\t'))
-                            {
-                                Console.WriteLine("ERROR: Bad Write Transaction Format");
-                                return;
-                            }
-
-                            //////////////////////////////////////
-                            i++;
-                            if (i >= newline.Length)
-                            {
-                                Console.WriteLine("ERROR: Bad Write Transaction Format");
-                                return;
-                            }
-                        }
-                        foreach(DadInt a in writes) Console.WriteLine(a.ToString());
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("ERROR: Bad Write Transaction Format");
-                        return;
-                    }
-                }
-			}
-			else if (newline[i] == ' ' || newline[i] == '\t'){ } // does nothing
-			else
-			{
-                Console.WriteLine("ERROR: Bad Transaction Format"); 
-				return;
-            }
-			i++;
-
-        }
-        List<DadInt> reply = _dadtkv.TxSubmit(reads, writes);
+        if(!Parse_Lib.Parse.ParseReads(newline, ref i,out reads)) { Console.WriteLine("ERROR: Bad Read Transaction Format"); return; }
+        foreach(string s in reads) Console.WriteLine(s);
+        if (!Parse_Lib.Parse.ParseWrites(newline, ref i, out writes)) { Console.WriteLine("ERROR: Bad Write Transaction Format"); return; }
+        foreach (KeyValuePair<string,int> a in writes) Console.WriteLine(a);
+        
+        List<DadInt> reply = _dadtkv.TxSubmit(reads, DictToList(writes));
         Console.WriteLine("Reads replies:");
         foreach (DadInt rep in reply) { Console.WriteLine(rep.ToString()); }
         return;
-        //Todo
+    }
+    private List<DadInt> DictToList(Dictionary<string,int> writes)
+    {
+        List<DadInt> dadInts = new List<DadInt>();
+        foreach (KeyValuePair<string, int> dad in writes) { dadInts.Add(new DadInt(dad.Key, dad.Value)); }
+        return dadInts;
     }
 }
--- Parse_Lib/Parse.cs
+++ Parse_Lib/Parse.cs
@@ -16,11 +16,106 @@
                 return -1;
             }
         }
-        public static string ParseString()
+        public static string ParseStringNoSpaces()
         {
-
-            return "";
+            string newline = Console.ReadLine();
+            string reduced = "";
+            if( newline == null ) { return ""; }
+            int i=0;
+            bool ask=false;
+            while(i < newline.Length) 
+            {
+                if (!ask && newline[i] == '"') ask = true;
+                else if (ask && newline[i] == '"') ask = false;
+                else if (!ask && (newline[i] == ' ' || newline[i] == '\t')) { i++; continue; }
+                reduced += newline[i];
+                i++;
+            }
+            return reduced;
         }
+        public static bool ParseReads(string newline, ref int i, out List<string> reads) 
+        {
+            reads = new List<string>();
+            if (newline[i] != '('){return false;}
+            i++;
+            while (i < newline.Length && newline[i] != ')') 
+            {
+                string name = "";
+                if(!ParseString(newline, ref i, ref name)) return false;
+                reads.Add(name);
 
+                if (newline[i] == ',') { i++; }
+                else if (newline[i] != ')') return false;
+            }
+            if (i >= newline.Length) return false;
+            i++;
+            if (i >= newline.Length) return false;
+            return true;
+        }
+        public static bool ParseWrites(string newline, ref int i, out Dictionary<string, int> writes)
+        {
+            writes = new Dictionary<string, int>();
+            if (newline[i] != '(') { return false; }
+            i++;
+            while (i < newline.Length && newline[i] != ')')
+            {
+                string name;
+                int val;
+                if (!ParseDict(newline, ref i, out name, out val)) return false;
+                writes.Add(name,val);
+
+                if (newline[i] == ',') { i++; }
+                else if (newline[i] != ')') return false;
+            }
+            if (i >= newline.Length) return false;
+            return true;
+        }
+        public static bool ParseDict(string newline, ref int i, out string name, out int val)
+        {
+            name = "";
+            val = 0;
+            if (newline[i] != '<') { return false; }
+            i++;
+            if (!ParseString(newline,ref i, ref name)) return false;
+            if (newline[i] != ',') { return false; }
+            if (++i >= newline.Length) return false;
+            if (!ParseNumber(newline, ref i, ref val)) return false;            
+            return true;
+        }
+        public static bool ParseNumber(string newline, ref int i, ref int val)
+        {
+            bool neg = false;
+            if (newline[i] == '-')
+            {
+                neg = true;
+                if (++i >= newline.Length) return false;
+            }
+            if (newline[i] == '>') { return false; }
+            while (i < newline.Length && newline[i] != '>') 
+            {
+                if (Char.IsNumber(newline[i])) val = val * 10 + newline[i] - '0';
+                else return false;
+                i++;
+            }
+            if (i >= newline.Length) return false;
+            i++;
+            if (i >= newline.Length) return false;
+            if (neg) val = -val;
+            return true;
+        }
+        public static bool ParseString(string newline, ref int i, ref string name)
+        {
+            if (newline[i] != '"') { return false; }
+            i++;
+            while (i < newline.Length && newline[i] != '"') 
+            {
+                name += newline[i];
+                i++;
+            }
+            if(i >= newline.Length) return false;
+            i++;
+            if(i >= newline.Length) return false;
+            return true;
+        }
     }
 }
