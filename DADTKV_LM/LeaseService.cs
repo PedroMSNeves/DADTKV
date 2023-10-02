3ee3a31e760370acd1570a860779ed0b9a499577
YHLQMDLG27 <84636429+YHLQMDLG27@users.noreply.github.com>
2023-10-02 10:55:55 +0100

    LM: basic structure

---
+++ DADTKV_LM/LeaseService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections;
+namespace DADKTV_LM
+{
+    public class ServerService : LeaseService.LeaseServiceBase
+    {
+        private string _name;
+        public ServerService(string name, List<string> tm_urls, List<string> lm_urls)
+        {
+            _name = name;
+        }
+
+        public override Task<LeaseReply> Lease(LeaseRequest request, ServerCallContext context) // return task
+        {
+            return Task.FromResult(Ls(request));
+        }
+        public LeaseReply Ls(LeaseRequest request) //perform the request
+        {
+            LeaseReply reply = new LeaseReply { };
+            return reply;
+        }
+    }
+}
