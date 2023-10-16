3917279740a063372e553b2c2cb6c72d8f9ea2da
PedroMSNeves <pedro.santos.neves@tecnico.ulisboa.pt>
2023-10-16 20:41:09 +0100

    fix: eliminate useless comments

--- DADTKV_TM/MainThread.cs
+++ DADTKV_TM/MainThread.cs
@@ -9,18 +9,7 @@
         }
         public void cycle()
         {
-            while (true)
-            {
-                /*
-                 * Verificar todos os requests
-                 * Retirar o primeiro request se possivel
-                 * 
-                 */
-                _store.Verify(); // Verificar todos os requests e mete os a S T ou N
-                                 // tenta executar 1 pedido (o primeiro ou algum depois caso nao tenha intersessoes)(de momento vai ser so o primeiro)
-                 //Thread.Sleep(100);
-
-            }
+            while (true) _store.Verify();
         }
     }
 }
