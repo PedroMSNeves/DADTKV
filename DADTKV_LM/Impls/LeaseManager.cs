e11f95f796d9abad4564f2b47b9547c896e92ed1
YHLQMDLG27 <84636429+YHLQMDLG27@users.noreply.github.com>
2023-10-22 17:27:09 +0100

    Feat: 2 phase commit

--- DADTKV_LM/Impls/LeaseManager.cs
+++ DADTKV_LM/Impls/LeaseManager.cs
@@ -12,12 +12,14 @@ namespace DADTKV_LM.Impls
         private LeaseData _data;
         LmContact _lmcontact;
         TmContact _tmContact;
+        private Dictionary<string, Request> received;
         public LeageManager(string name, LeaseData data, List<string> tm_urls, List<string> lm_urls)
         {
             _name = name;
             _data = data;
             _tmContact = new TmContact(tm_urls);
             _lmcontact = new LmContact(name, lm_urls);
+            received = new Dictionary<string, Request>();
         }
 
         public override Task<LeaseReply> Lease(LeaseRequest request, ServerCallContext context)
@@ -27,8 +29,37 @@ namespace DADTKV_LM.Impls
         public LeaseReply Ls(LeaseRequest request)
         {
             Console.WriteLine("NEW REQUEST: " + request.ToString());
-            _data.AddRequest(new Request(request.Id, request.Keys.ToList(), request.LeaseId));
+            received.Add(request.Id, new Request(request.Id, request.Keys.ToList(), request.LeaseId));
+            //_data.AddRequest(new Request(request.Id, request.Keys.ToList(), request.LeaseId));
             return new LeaseReply { Ack = true };
+        }
+
+        public override Task<LeaseReply> ConfirmLease(ConfirmLeaseRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(Confirm(request));
+        }
+        public LeaseReply Confirm(ConfirmLeaseRequest request)
+        {
+            if (request.Ack)
+            {
+                Request req;
+                if (received.TryGetValue(request.Id, out req))
+                {
+                    _data.AddRequest(req);
+                    received.Remove(request.Id);
+                }
+            }
+            else
+            {
+                Request req;
+                if (received.TryGetValue(request.Id, out req))
+                {
+                    _data.AddRequest(req);
+                    received.Remove(request.Id);
+                }
+            }
+            return new LeaseReply { Ack = true };
+
         }
 
         public void BroadcastLease(int epoch, List<Request> leases)
