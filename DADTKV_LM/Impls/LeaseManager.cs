b1b00037e23b2c3b61c361efc16bd7f4e6eefe64
YHLQMDLG27 <84636429+YHLQMDLG27@users.noreply.github.com>
2023-10-23 21:05:23 +0100

    fix: not using 2 phase commit in the msg between tm and lm

--- DADTKV_LM/Impls/LeaseManager.cs
+++ DADTKV_LM/Impls/LeaseManager.cs
@@ -12,14 +12,12 @@ namespace DADTKV_LM.Impls
         private LeaseData _data;
         LmContact _lmcontact;
         TmContact _tmContact;
-        List<Request> received;
         public LeageManager(string name, LeaseData data, List<string> tm_urls, List<string> lm_urls)
         {
             _name = name;
             _data = data;
             _tmContact = new TmContact(tm_urls);
             _lmcontact = new LmContact(name, lm_urls);
-            received = new List<Request>();
         }
 
         public override Task<LeaseReply> Lease(LeaseRequest request, ServerCallContext context)
@@ -29,38 +27,9 @@ namespace DADTKV_LM.Impls
         public LeaseReply Ls(LeaseRequest request)
         {
             Console.WriteLine("NEW REQUEST: " + request.ToString());
-            lock(received)
-            {
-                received.Add(new Request(request.Id, request.Keys.ToList(), request.LeaseId));
-            }
-            //_data.AddRequest(new Request(request.Id, request.Keys.ToList(), request.LeaseId));
+
+            _data.AddRequest(new Request(request.Id, request.Keys.ToList(), request.LeaseId));
             return new LeaseReply { Ack = true };
-        }
-
-        public override Task<LeaseReply> ConfirmLease(ConfirmLeaseRequest request, ServerCallContext context)
-        {
-            return Task.FromResult(Confirm(request));
-        }
-        public LeaseReply Confirm(ConfirmLeaseRequest request)
-        {
-            Request req = null;
-
-            lock(received)
-            {
-                foreach (Request r in received)
-                {
-                    if (r.Lease_ID == request.LeaseId && r.Tm_name == request.Id)
-                    {
-                        req = r;
-                    }
-                }
-                if(req == null) return new LeaseReply { Ack = true };
-                if (request.Ack) _data.AddRequest(req);
-                received.Remove(req);
-            }
-            Console.WriteLine("CONFIRMATION TM : " + request.Id + " LEASEID: " + request.LeaseId + request.Ack);
-            return new LeaseReply { Ack = true };
-
         }
 
         public void BroadcastLease(int epoch, List<Request> leases)
